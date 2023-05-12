851d34177fcfa86d076343f6e9752098daa0d000
oscarsanchog <oscar.sancho.gonzalez@gmail.com>
2023-05-12 12:28:14 -0400

    primer commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,24 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let suma = 0
+   let array = num.split('') // para convertir de un string a un array
+   let arrayReverse = array.reverse() // para dar vuelta ese reverse
+   for (let i = 0; i < num.length; i++) { // para recorrer el array
+      suma += arrayReverse[i] * 2 ** i // Esta es la fórmula
+   }
+   return suma
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let binario = []
+   while (num >= 1) {
+      let resto = Math.trunc(num % 2)
+      num = num / 2
+      binario.unshift(resto)
+   }
+return binario.join('')
+}
 
 module.exports = {
    BinarioADecimal,
